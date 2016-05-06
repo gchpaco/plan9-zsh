9316eae97ca868447c5db4e55346e61fa6bd4676
Graham Hughes <ghughes@meteor.com>
2016-05-06 16:44:31 -0700

    Copy auchter's plan9 plugin

---
+++ plan9.plugin.zsh
@@ -0,0 +1,20 @@
+[[ -d /opt/plan9 ]] && export PLAN9=/opt/plan9
+[[ -d /usr/lib/plan9 ]] && export PLAN9=/usr/lib/plan9
+[[ -d /usr/local/plan9 ]] && export PLAN9=/usr/local/plan9
+
+if [[ -d $PLAN9 ]]; then
+   export NAMESPACE=/tmp/ns.$USER.`hostname`
+   export PATH=$PATH:$PLAN9/bin
+
+   [[ ! -d $NAMESPACE ]] && mkdir $NAMESPACE
+fi
+
+# Some installations don't include plumber...
+if [[ -e $PLAN9/bin/plumber ]]; then
+   9p read plumb/rules >/dev/null 2>/dev/null
+   [[ $? -ne 0 ]] && plumber 2>/dev/null
+
+   # first rule wins, so make sure files are named in proper order
+   cat $ZSH/plan9/*.plumbing $PLAN9/plumb/initial.plumbing | 9p write plumb/rules >/dev/null
+fi
+
